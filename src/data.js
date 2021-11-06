e669f61cd40c29dbc66da6723da6486f6e693ff4
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-11-06 16:50:35 +0100

    React Dark Mode Tutorial (Context API)

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const products = [
+	{
+		id: 1,
+		img: 'https://i.ibb.co/njMBFZm/Screenshot-2021-11-06-at-12-23-47-Miguel-Dominguez-Portfolio.png',
+		link: 'http://lama.dev',
+	},
+	{
+		id: 2,
+		img: 'https://i.ibb.co/wLRrQYY/Screenshot-2021-11-06-at-12-24-17-Manuel-de-Arpe.png',
+		link: 'http://lama.dev',
+	},
+	{
+		id: 3,
+		img: 'https://i.ibb.co/XWs6nHt/Screenshot-2021-11-06-at-12-24-42-Pepe-Navas.png',
+		link: 'http://lama.dev',
+	},
+	{
+		id: 4,
+		img: 'https://i.ibb.co/TKt5gSK/Screenshot-2021-11-06-at-12-25-12-Owl-of-Minerva.png',
+		link: 'http://lama.dev',
+	},
+	{
+		id: 5,
+		img: 'https://i.ibb.co/wLRrQYY/Screenshot-2021-11-06-at-12-24-17-Manuel-de-Arpe.png',
+		link: 'http://lama.dev',
+	},
+	{
+		id: 6,
+		img: 'https://i.ibb.co/njMBFZm/Screenshot-2021-11-06-at-12-23-47-Miguel-Dominguez-Portfolio.png',
+		link: 'http://lama.dev',
+	},
+]
+
+/*
+https://i.ibb.co/njMBFZm/Screenshot-2021-11-06-at-12-23-47-Miguel-Dominguez-Portfolio.png
+https://i.ibb.co/wLRrQYY/Screenshot-2021-11-06-at-12-24-17-Manuel-de-Arpe.png
+https://i.ibb.co/XWs6nHt/Screenshot-2021-11-06-at-12-24-42-Pepe-Navas.png
+https://i.ibb.co/TKt5gSK/Screenshot-2021-11-06-at-12-25-12-Owl-of-Minerva.png
+*/
+
+/*
+https://i.ibb.co/94LSscV/6.jpg
+https://i.ibb.co/2MCvH1c/5.jpg
+https://i.ibb.co/DGPwM8G/4.jpg
+https://i.ibb.co/6syzP3K/3.jpg
+https://i.ibb.co/qxvxQpD/2.jpg
+https://i.ibb.co/cFW59n2/1.jpg
+*/
+
+/*
+https://ibb.co/7p5L2jR
+https://ibb.co/6tTHL7D
+https://ibb.co/f8VrNn8
+https://ibb.co/dKfF2dN
+https://ibb.co/yVrVjqN
+https://ibb.co/Nj0hqBW
+*/
+/*
+https://ibb.co/7p5L2jR
+https://ibb.co/6tTHL7D
+https://ibb.co/f8VrNn8
+https://ibb.co/dKfF2dN
+https://ibb.co/yVrVjqN
+https://ibb.co/Nj0hqBW
+*/
